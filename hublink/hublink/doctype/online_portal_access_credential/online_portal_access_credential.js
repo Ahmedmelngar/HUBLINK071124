16cae161657850b31f9685aecddeddf3725f8ba0
Ahmedmelngar <ahmed.m.elngar@gmail.com>
2024-11-07 15:32:37 +0400

    frist commit

---
+++ hublink/hublink/doctype/online_portal_access_credential/online_portal_access_credential.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, AHMED ELNAGAR and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Online Portal Access Credential", {
+// 	refresh(frm) {
+
+// 	},
+// });
