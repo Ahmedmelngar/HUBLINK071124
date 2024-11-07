bdd008024624c3fc7b287506eee87128220ea18d
Ahmedmelngar <ahmed.m.elngar@gmail.com>
2024-11-07 15:34:27 +0400

    frist commit

---
+++ hublink/hublink/report/permitted_documents_for_user/permitted_documents_for_user.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, AHMED ELNAGAR and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Permitted Documents For User"] = {
+	"filters": [
+
+	]
+};
